fdf02400ac290be97c67df259ea1cc4cf4a740d2
wxcican <wxcican@qq.com>
2017-06-23 14:32:22 +0800

    实现欢迎页，主页二次点击返回退出

---
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170623/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.fuicuiedu.xc.easyshop_20170623;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fuicuiedu.xc.easyshop_20170623.commons.ActivityUtils;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ActivityUtils activityUtils;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        activityUtils = new ActivityUtils(this);
+
+        // TODO: 2017/6/23 0023  判断用户是否登录，自动登录
+        // TODO: 2017/6/23 0023  环信登录相关（账号冲突踢出）
+
+        //1.5s跳转到主页
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //跳转到主页
+                activityUtils.startActivity(MainActivity.class);
+                finish();
+            }
+        }, 1500);
+    }
+}
