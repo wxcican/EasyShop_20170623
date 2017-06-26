00d9555b2d7f3c694f0c7ba296b54048a82556f9
wxcican <wxcican@qq.com>
2017-06-26 16:41:38 +0800

    添加日志拦截器

--- app/src/main/java/com/fuicuiedu/xc/easyshop_20170623/network/EasyShopClient.java
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170623/network/EasyShopClient.java
@@ -5,6 +5,7 @@ import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 /**
  * 作者：王小超
@@ -18,7 +19,15 @@ public class EasyShopClient {
     private OkHttpClient okHttpClient;
 
     private EasyShopClient(){
-        okHttpClient = new OkHttpClient();
+        //初始化日志拦截器
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        //设置拦截级别
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        okHttpClient = new OkHttpClient.Builder()
+                //添加日志拦截器
+                .addInterceptor(httpLoggingInterceptor)
+                .build();
     }
 
     public static EasyShopClient getInstance(){
@@ -26,6 +35,21 @@ public class EasyShopClient {
             easyShopClient = new EasyShopClient();
         }
         return easyShopClient;
+    }
+
+    //登录
+    public Call login(String username, String password){
+        RequestBody requestBody = new FormBody.Builder()
+                .add("username",username)
+                .add("password",password)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(EasyShopApi.BASE_URL + EasyShopApi.LOGIN)
+                .post(requestBody)
+                .build();
+
+        return okHttpClient.newCall(request);
     }
 
     //注册
@@ -36,7 +60,7 @@ public class EasyShopClient {
                 .build();
 
         Request request = new Request.Builder()
-                .url("http://wx.feicuiedu.com:9094/yitao/UserWeb?method=register")
+                .url(EasyShopApi.BASE_URL + EasyShopApi.REGISTER)
                 .post(requestBody)
                 .build();
 
--- app/src/main/java/com/fuicuiedu/xc/easyshop_20170623/user/login/LoginActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170623/user/login/LoginActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 
 import com.fuicuiedu.xc.easyshop_20170623.R;
 import com.fuicuiedu.xc.easyshop_20170623.commons.ActivityUtils;
+import com.fuicuiedu.xc.easyshop_20170623.network.EasyShopClient;
 import com.fuicuiedu.xc.easyshop_20170623.user.register.RegisterActivity;
 
 import java.io.IOException;
@@ -103,32 +104,17 @@ public class LoginActivity extends AppCompatActivity {
         switch (v.getId()){
             case R.id.btn_login:
 
-                OkHttpClient okHttpClient = new OkHttpClient();
 
-                RequestBody requestBody = new FormBody.Builder()
-                        .add("username",username)
-                        .add("password",password)
-                        .build();
-
-                Request request = new Request.Builder()
-                        .url("http://wx.feicuiedu.com:9094/yitao/UserWeb?method=login")
-                        .post(requestBody)
-                        .build();
-
-                okHttpClient.newCall(request).enqueue(new Callback() {
+                Call call = EasyShopClient.getInstance().login(username,password);
+                call.enqueue(new Callback() {
                     @Override
                     public void onFailure(Call call, IOException e) {
-                        Log.e("aaa","网络连接失败");
+
                     }
 
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
-                        Log.e("aaa","网络连接成功");
-                        if (response.isSuccessful()){
-                            Log.e("aaa","服务器响应成功");
-                        }else{
-                            Log.e("aaa","服务器响应失败");
-                        }
+
                     }
                 });
 
