f8f504d43ff40186f06c87df79e4e3b227964246
wxcican <wxcican@qq.com>
2017-07-05 11:13:14 +0800

    完成商品上传页面，Activity搭建01

--- app/src/main/java/com/fuicuiedu/xc/easyshop_20170623/main/me/goodsupload/GoodsUpLoadActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170623/main/me/goodsupload/GoodsUpLoadActivity.java
@@ -1,21 +1,38 @@
 package com.fuicuiedu.xc.easyshop_20170623.main.me.goodsupload;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fuicuiedu.xc.easyshop_20170623.R;
 import com.fuicuiedu.xc.easyshop_20170623.commons.ActivityUtils;
+import com.fuicuiedu.xc.easyshop_20170623.commons.MyFileUtils;
+import com.fuicuiedu.xc.easyshop_20170623.components.PicWindow;
+import com.fuicuiedu.xc.easyshop_20170623.components.ProgressDialogFragment;
+import com.fuicuiedu.xc.easyshop_20170623.model.ImageItem;
 import com.hannesdorfmann.mosby.mvp.MvpActivity;
 
-import butterknife.BindView;
+import java.io.File;
+import java.util.ArrayList;
 
-public class GoodsUpLoadActivity extends MvpActivity<GoodsUpLoadView,GoodsUpLoadPresenter> implements GoodsUpLoadView{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class GoodsUpLoadActivity extends MvpActivity<GoodsUpLoadView, GoodsUpLoadPresenter> implements GoodsUpLoadView {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -44,10 +61,49 @@ public class GoodsUpLoadActivity extends MvpActivity<GoodsUpLoadView,GoodsUpLoad
     private String str_goods_type = goods_type_num[0];//商品类型（默认家用）
     private String str_goods_describe;//商品描述
 
+    //模式：普通=1
+    public static final int MODE_DONE = 1;
+    //模式：删除=2
+    public static final int MODE_DELETE = 2;
+    private int title_mode = MODE_DONE;
+    private ArrayList<ImageItem> list = new ArrayList<>();
+    private GoodsUpLoadAdapter adapter;
+    private PicWindow picWindow;
+    private ProgressDialogFragment dialogFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_up_load);
+        ButterKnife.bind(this);
+        activityUtils = new ActivityUtils(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        initView();
+    }
+
+    private void initView() {
+        picWindow = new PicWindow(this, listener);
+
+        //RecyclerView
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        //设置默认动画（item增删动画）
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        //设置固定大小
+        recyclerView.setHasFixedSize(true);
+
+        //获取缓存文件夹中的图片文件
+        list = getFilePhoto();
+        adapter = new GoodsUpLoadAdapter(this,list);
+        adapter.setListener(itemClickListener);
+        recyclerView.setAdapter(adapter);
+
+        //商品名称，价格，描述输入监听
+        et_goods_name.addTextChangedListener(textWatcher);
+        et_goods_price.addTextChangedListener(textWatcher);
+        et_goods_describe.addTextChangedListener(textWatcher);
     }
 
     @NonNull
@@ -56,6 +112,83 @@ public class GoodsUpLoadActivity extends MvpActivity<GoodsUpLoadView,GoodsUpLoad
         return new GoodsUpLoadPresenter();
     }
 
+    //toolbar返回要实现的方法
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+    //图片选择弹窗内的监听事件
+    private PicWindow.Listener listener = new PicWindow.Listener() {
+        @Override
+        public void toGallery() {
+            // TODO: 2017/7/5 0005 相册
+        }
+
+        @Override
+        public void toCamera() {
+            // TODO: 2017/7/5 0005 相机
+        }
+    };
+
+    //获取缓存文件夹中的文件
+    public ArrayList<ImageItem> getFilePhoto() {
+        ArrayList<ImageItem> items = new ArrayList<>();
+        //拿到所有图片文件
+        File[] files = new File(MyFileUtils.SD_PATH).listFiles();
+        if (files != null) {
+            for (File file : files) {
+                //解码file拿到bitmap
+                Bitmap bitmap = BitmapFactory.decodeFile(MyFileUtils.SD_PATH + file.getName());
+                ImageItem item = new ImageItem();
+                item.setImagePath(file.getName());
+                item.setBitmap(bitmap);
+                items.add(item);
+            }
+        }
+        return items;
+    }
+
+    //适配器中自定义的监听事件
+    private GoodsUpLoadAdapter.OnItemClickedListener itemClickListener = new GoodsUpLoadAdapter.OnItemClickedListener() {
+        @Override
+        public void onAddClicked() {
+            // TODO: 2017/7/5 0005 相关实现，待完成
+        }
+
+        @Override
+        public void onPhotoClicked(ImageItem photo, ImageView imageView) {
+
+        }
+
+        @Override
+        public void onLongClicked() {
+
+        }
+    };
+
+    //商品名称，价格，描述输入监听
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            str_goods_name = et_goods_name.getText().toString();
+            str_goods_price = et_goods_price.getText().toString();
+            str_goods_describe = et_goods_describe.getText().toString();
+            //判断上传按钮是否可点击
+            boolean can_save = !(TextUtils.isEmpty(str_goods_name) || TextUtils.isEmpty(str_goods_price)
+                    || TextUtils.isEmpty(str_goods_describe));
+            btn_goods_load.setEnabled(can_save);
+        }
+    };
 
     // ###################################  视图接口相关   #################
     @Override
