67a76a4a60498fb0a49da6a516ad3f6bd810eb34
wxcican <wxcican@qq.com>
2017-07-06 14:31:40 +0800

    加入环信模块，并初始化

--- app/src/main/java/com/fuicuiedu/xc/easyshop_20170623/network/EasyShopClient.java
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170623/network/EasyShopClient.java
@@ -7,6 +7,7 @@ import com.google.gson.Gson;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.FormBody;
@@ -196,4 +197,38 @@ public class EasyShopClient {
 
         return okHttpClient.newCall(request);
     }
+
+    //查找好友
+    public Call getSearchUser(String nickname){
+        RequestBody requestBody = new FormBody.Builder()
+                .add("nickname",nickname)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(EasyShopApi.BASE_URL + EasyShopApi.GET_USER)
+                .post(requestBody)
+                .build();
+
+        return okHttpClient.newCall(request);
+    }
+
+    //获取好友列表
+    public Call getUsers(List<String> ids){
+        String names = ids.toString();
+        //清楚list转换后的String空格
+        names = names.replace(" ","");
+
+        RequestBody requestBody = new FormBody.Builder()
+                .add("name",names)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(EasyShopApi.BASE_URL + EasyShopApi.GET_NAMES)
+                .post(requestBody)
+                .build();
+
+        return okHttpClient.newCall(request);
+    }
+
+
 }
